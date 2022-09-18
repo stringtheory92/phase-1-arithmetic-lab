d88135f74d0d965ef3bd2c4ddb62f5fd7f086a81
Adam Kabak <adamkabak@yahoo.com>
2022-09-18 17:38:28 -0400

    commit tree 633c699bed118db15b7d6b1db0bf17fd0b2263b8

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// function multiply() {
+//     const num1 = 62
+//     const num2 = 1
+//     return num1 * num2;
+// }
+const num1 = 62
+const num2 = 1
+const num3 = 14
+const num4 = 10
+const set = [20]
+let multiply = num1 * num2;
 
+const random = Math.ceil((Math.random() * 10) + 1)
+
+const mod = num3 % num4;
+
+const max = Math.max(set)
 // Write your code here
+console.log(random());
+console.log(multiply());
